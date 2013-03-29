fd14ad8fcdef0e68e564e10665558f95112d635f
Mark and India <mark+india@appacademy.io>
2013-03-28 17:25:20 -0700

    Update QuestionFollower class.

--- question_followers.rb
+++ question_followers.rb
@@ -2,23 +2,28 @@ require 'sqlite3'
 require_relative 'aa_questions'
 
 class QuestionFollower
-  #
-  # def self.get_followers_by_question_id(question_id)
-  #   query = "SELECT user_id FROM question_followers WHERE question_id = #{question_id}"
-  #   result = AA_QuestionsDatabase.instance.execute(query)
-  #   return [] if result.empty?
-  #
-  #   followers = []
-  #   result.each { |row| followers << User.parse(row) }
-  #
-  #   followers
-  # end
-  #
-  # def self.parse(result)
-  #   follower =
-  #
-  # end
+  attr_accessor :id, :question_id, :follower_id
 
+  def self.find_all
+    query = "SELECT * FROM question_followers"
+    result = QuestionsDatabase.instance.execute(query)
+  end
 
+  def self.find_by_id(id)
+    query = "SELECT * FROM question_followers WHERE id = #{id}"
+    row_hash_result = QuestionsDatabase.instance.execute(query)
+    row_hash_result.empty? ? nil : parse(row_hash_result[0])
+  end
+
+  def initialize(options = {})
+    @id = options[:id]
+    @question_id, @follower_id = options[:question_id], options[:follower_id]
+  end
+
+  def self.parse(row_hash)
+    question_follower = QuestionFollower.new(id: row_hash["id"],
+                            question_id: row_hash["question_id"],
+                            follower_id: row_hash["user_id"])
+  end
 
 end
